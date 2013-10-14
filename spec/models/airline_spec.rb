2e199575c222118683ca81eb07d54a603406260a
samspector <samalden.spector@Gmail.com>
2013-10-14 13:18:18 -0400

    adding airline specs

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe Airline do 
 	before :each do 
-
 	end
 end
 
+
+## these are the things being created in each airline object
+#		(id: integer, name: string, created_at: datetime, updated_at: datetime)
  
