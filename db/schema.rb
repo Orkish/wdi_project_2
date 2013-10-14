76db65d0969669e1ac2c78d10e3b0d90d82f2d3e
Orkish <winson.yuan@hotmail.com>
2013-10-14 12:30:43 -0400

    schema update?

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131014143836) do
+ActiveRecord::Schema.define(:version => 20131014152731) do
 
   create_table "airlines", :force => true do |t|
-    t.string   "name"
+    t.string   "name",       :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "airlines_airports", :id => false, :force => true do |t|
+    t.integer  "airline_id"
+    t.integer  "aitport_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
