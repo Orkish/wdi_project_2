68c23f9d1ae3024e8db2b8e7df6c2285a0f350d3
Orkish <winson.yuan@hotmail.com>
2013-10-14 17:45:49 -0400

    update

--- spec/models/airport_spec.rb
+++ spec/models/airport_spec.rb
@@ -1,5 +1,19 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe Airport do
-#   before :each do
-#     @airport = Airport.new 
+describe Airport do
+  before :each do
+    @airport = Airport.new(name: "EWR")
+    @flight = Flight.new(city: "Saint Louis", country: "US", continent: "NA", departure: "2000-01-01 00:00:00", airport_id: "1")
+  end
+
+  describe "#name" do
+    it "should return the correct airport name" do
+      @airport.name.should eq("EWR")
+    end
+  end
+
+  it "should receive a flight association" do
+    @airport.flights << @flight
+    @airport.flights.first.should eq(@flight)
+  end
+end
