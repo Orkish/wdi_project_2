3cb5dfc247316b3454681e8f715bc4600213188a
whyu <wesleyhl.yu@gmail.com>
2013-10-14 23:56:33 -0400

    modified test.html.erb and the controller to do some ajax-json stuff

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,13 +1,30 @@
 $(function(){
-   $("h1").hide();
-   setTimeout(function(){
-    $("h1").fadeIn("slow");
-   }, 1500);
-});
+  $("h1").hide();
+  setTimeout(function(){
+  	$("h1").fadeIn("slow");
+  }, 1500);
 
-$(function(){
   $("body").hide();
   setTimeout(function(){
-    $("body").fadeIn(2000);
+  	$("body").fadeIn(2000);
   }, 1500);
+
+  $("#ajax-one").on("click", function(){
+  	$.ajax({
+  		url: '/flights',
+  		method: 'GET',
+  		dataType: 'json'
+  	})
+  	.done(function(data) {
+  		console.log(data);
+  		$('#response-one').append("<li>" + data.city + "</li>");
+  		$('#response-one').append("<li>" + data.country + "</li>");
+  		$('#response-one').append("<li>" + data.continent + "</li>");
+  		$('#response-one').append("<li>" + data.departure + "</li>");
+  	})
+
+  })
+
+
+
 });
