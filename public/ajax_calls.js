3c8c129c209eb6449f6dd0d72e001e0678670890
whyu <wesleyhl.yu@gmail.com>
2013-10-15 12:18:33 -0400

    added some ideas to ajax_calls.js in public folder

--- public/ajax_calls.js
+++ public/ajax_calls.js
@@ -6,10 +6,31 @@ load index start animation
 
 
 var data = {
-  0: {}
-  1: {}
-  2: []
-  3: []
+  flight_0: [],
+  flight_1: [],
+  flight_2: [],
+  flight_3: [],
+  flight_4: [],
+  flight_5: [],
+  flight_6: [],
+  flight_7: [],
+  flight_8: [],
+  flight_9: [],
+  flight_10: [],
+  flight_11: [],
+  flight_12: [],
+  flight_13: [],
+  flight_14: [],
+  flight_15: [],
+  flight_16: [],
+  flight_17: [],
+  flight_18: [],
+  flight_19: [],
+  flight_20: [],
+  flight_21: [],
+  flight_22: [],
+  flight_23: [],
+  flight_24: [],
 }
 
 var index = 0;
