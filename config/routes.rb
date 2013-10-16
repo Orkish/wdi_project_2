90f9cc77ea64d631f9e19229701643b7f65b7f8f
samspector <samalden.spector@Gmail.com>
2013-10-15 20:54:25 -0400

    devise up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ProjectTwo::Application.routes.draw do
+  devise_for :administrators
+
+  devise_for :admins
+
   root to: 'flights#index'
   get '/flights', to: "flights#test", as: "flights"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,50 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131015202131) do
+ActiveRecord::Schema.define(:version => 20131016004843) do
+
+  create_table "administrators", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "administrators", ["email"], :name => "index_administrators_on_email", :unique => true
+  add_index "administrators", ["reset_password_token"], :name => "index_administrators_on_reset_password_token", :unique => true
+
+  create_table "admins", :force => true do |t|
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                                          :null => false
+    t.datetime "updated_at",                                          :null => false
+  end
+
+  add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
+  add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
 
   create_table "airlines", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
-  add_index "airlines", ["name"], :name => "index_airlines_on_name", :unique => true
 
   create_table "airlines_airports", :id => false, :force => true do |t|
     t.integer "airline_id"
